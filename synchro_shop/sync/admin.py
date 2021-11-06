812a62dec8a9add9c42becf3fc817397eb26d382
Jean Luc SEMEDO <francisco@it4life.org>
2021-11-06 14:05:42 +0000

    FEAT: Add django admin configuration

--- synchro_shop/sync/admin.py
+++ synchro_shop/sync/admin.py
@@ -1,3 +1,19 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib import admin as auth_admin
 
-# Register your models here.
+from .models import Shop, ShopReading
+
+
+@admin.register(Shop)
+class ShopAdmin(auth_admin.ModelAdmin):
+    list_display = ["id", "uuid", "name"]
+    search_fields = ["name"]
+
+
+@admin.register(ShopReading)
+class ShopReadingAdmin(auth_admin.ModelAdmin):
+    list_display = ["gtin", "get_shop", "expiry_date", "reading_time"]
+    search_fields = ["gtin"]
+
+    def get_shop(self, obj):
+        return obj.shop.name
