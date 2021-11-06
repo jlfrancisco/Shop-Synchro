be37d70feabfc9e32ab1c030f3446a04a14c646d
Jean Luc SEMEDO <francisco@it4life.org>
2021-11-06 14:13:04 +0000

    FIX: Cleanup

--- synchro_shop/conftest.py
+++ synchro_shop/conftest.py
@@ -2,18 +2,11 @@ import pytest
 
 from synchro_shop.sync.models import Shop
 from synchro_shop.sync.tests.factories import ShopFactory
-from synchro_shop.users.models import User
-from synchro_shop.users.tests.factories import UserFactory
 
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
-@pytest.fixture
-def user() -> User:
-    return UserFactory()
 
 
 @pytest.fixture()
