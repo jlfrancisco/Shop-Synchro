6193793b18725284bd7681270f58d0339aa8a969
Jean Luc SEMEDO <francisco@it4life.org>
2021-11-06 10:35:39 +0000

    INIT: Initialize django fs with cookiecutter

---
+++ synchro_shop/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from synchro_shop.users.models import User
+from synchro_shop.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
